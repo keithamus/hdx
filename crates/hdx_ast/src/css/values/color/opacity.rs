e1c12c64941d7feebe256e99c6160c02999a2ecf
Keith Cirkel <keithamus@users.noreply.github.com>
2024-03-03 23:20:14 +0000

    add opacity

--- crates/hdx_ast/src/css/values/color/opacity.rs
+++ crates/hdx_ast/src/css/values/color/opacity.rs
@@ -1,1 +1,37 @@
-pub type Opacity = super::super::Todo;
+use crate::css::values::units::{CSSFloat, Percent};
+use crate::{Parsable, Value, Writable};
+
+// https://drafts.csswg.org/css-color/#transparency
+#[derive(Value, Parsable, Writable, Debug, PartialEq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize), serde(tag = "type", rename_all = "kebab-case"))]
+pub enum Opacity {
+	#[parsable(Number, Check::Range(0.0..=1.0))]
+	Number(CSSFloat),
+	#[parsable(Dimension, Check::Range(0.0..=100.0), atom = "%")]
+	Percentage(Percent),
+}
+
+impl Default for Opacity {
+    fn default() -> Self {
+        Self::Number(1.0.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::test_helpers::*;
+
+	#[test]
+	fn size_test() {
+		assert_size!(Opacity, 8);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(Opacity, "0");
+		assert_parse!(Opacity, "1");
+		assert_parse!(Opacity, "100%");
+		assert_parse!(Opacity, "0.9999");
+	}
+}
