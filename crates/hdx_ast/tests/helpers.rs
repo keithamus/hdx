2ffce166dcd6ad757717ff3044c08707ecb6670e
Keith Cirkel <keithamus@users.noreply.github.com>
2024-05-04 09:30:09 +0100

    rewrite ast coverage tests

---
+++ crates/hdx_ast/tests/helpers.rs
@@ -0,0 +1,21 @@
+#[macro_export]
+macro_rules! assert_snap_ast {
+	($source_path: literal) => {
+		{
+			use bumpalo::Bump;
+			use hdx_ast::css::StyleSheet;
+			use hdx_parser::{Features, Parser};
+			use std::{fs::read_to_string};
+
+			let allocator = Bump::default();
+			let source_text = read_to_string($source_path).unwrap();
+			let parser = Parser::new(&allocator, &source_text, Features::default());
+			let result = parser.parse_entirely_with::<StyleSheet>();
+			if !result.errors.is_empty() {
+				panic!("\n\nParse {:?} failed. Saw error {:?}", $source_path, result.errors[0]);
+			}
+			insta::assert_json_snapshot!(result.output.unwrap())
+		}
+	}
+}
+
