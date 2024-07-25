ebc79c19de5f6afca060cf9b0695f5a18fd03e98
Kristján Oddsson <hi@koddsson.com>
2024-07-25 14:47:05 +0200

    try a method for matching delimiters

--- crates/hdx_parser/src/comparison.rs
+++ crates/hdx_parser/src/comparison.rs
@@ -1,5 +1,5 @@
 use crate::{unexpected, Parse, Parser, Result};
-use hdx_lexer::{Include, Token};
+use hdx_lexer::{Include, Kind};
 
 #[derive(Debug, PartialEq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize), serde(tag = "type"))]
@@ -13,25 +13,29 @@ pub enum Comparison {
 
 impl<'a> Parse<'a> for Comparison {
 	fn parse(parser: &mut Parser<'a>) -> Result<Comparison> {
-		Ok(match parser.next() {
-			Token::Delim('=') => Comparison::Equal,
-			Token::Delim('>') => {
-				if let Token::Delim('=') = parser.peek_with(Include::Whitespace) {
+		Ok(match (parser.next().kind(), parser.next().char()) {
+			(Kind::Delim, Some('=')) => Comparison::Equal,
+			(Kind::Delim, Some('>')) => {
+				if let (Kind::Delim, Some('=')) =
+					(parser.peek_with(Include::Whitespace).kind(), parser.peek_with(Include::Whitespace).char())
+				{
 					parser.advance_with(Include::Whitespace);
 					Comparison::GreaterThanEqual
 				} else {
 					Comparison::GreaterThan
 				}
 			}
-			Token::Delim('<') => {
-				if let Token::Delim('=') = parser.peek_with(Include::Whitespace) {
+			(Kind::Delim, Some('<')) => {
+				if let (Kind::Delim, Some('=')) =
+					(parser.peek_with(Include::Whitespace).kind(), parser.peek_with(Include::Whitespace).char())
+				{
 					parser.advance_with(Include::Whitespace);
 					Comparison::LessThanEqual
 				} else {
 					Comparison::LessThan
 				}
 			}
-			token => unexpected!(parser, token),
+			(token, _) => unexpected!(parser, token),
 		})
 	}
 }
