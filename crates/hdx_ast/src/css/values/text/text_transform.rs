669164a311e15067db05052866bc10dfca156db0
Keith Cirkel <keithamus@users.noreply.github.com>
2024-07-28 23:53:45 +0100

    remove some warnings

--- crates/hdx_ast/src/css/values/text/text_transform.rs
+++ crates/hdx_ast/src/css/values/text/text_transform.rs
@@ -33,7 +33,7 @@ impl<'a> Parse<'a> for TextTransform {
 	fn parse(parser: &mut Parser<'a>) -> ParserResult<Self> {
 		let mut value = Self::none();
 		loop {
-			if value.is_all() {
+			if value.is_all_bits() {
 				break;
 			}
 			match parser.peek() {
--- crates/hdx_ast/src/css/values/text/white_space_trim.rs
+++ crates/hdx_ast/src/css/values/text/white_space_trim.rs
@@ -22,7 +22,7 @@ impl<'a> Parse<'a> for WhiteSpaceTrim {
 	fn parse(parser: &mut Parser<'a>) -> ParserResult<Self> {
 		let mut value = Self::none();
 		loop {
-			if value.is_all() {
+			if value.is_all_bits() {
 				break;
 			}
 			match parser.next() {
