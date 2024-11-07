9244010528e2949efcb272e6fd562b454530f9b0
Keith Cirkel <keithamus@users.noreply.github.com>
2024-11-07 21:44:08 +0000

    (ast/values) position tests

--- crates/hdx_ast/src/css/values/position/impls.rs
+++ crates/hdx_ast/src/css/values/position/impls.rs
@@ -1,2 +1,34 @@
 pub(crate) use crate::traits::StyleValue;
 pub(crate) use hdx_proc_macro::*;
+
+
+#[cfg(test)]
+mod tests {
+	use super::super::*;
+	use crate::test_helpers::*;
+
+	#[test]
+	pub fn size_test() {
+		assert_size!(Position, 1);
+		assert_size!(Top, 8);
+		assert_size!(Right, 8);
+		assert_size!(Bottom, 8);
+		assert_size!(Left, 8);
+		assert_size!(InsetBlockStart, 8);
+		assert_size!(InsetInlineStart, 8);
+		assert_size!(InsetBlockEnd, 8);
+		assert_size!(InsetInlineEnd, 8);
+		assert_size!(InsetBlock, 16);
+		assert_size!(InsetInline, 16);
+		assert_size!(Inset, 32);
+		assert_size!(Overlay, 1);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(Position, "sticky");
+		assert_parse!(InsetBlockStart, "auto");
+		assert_parse!(Inset, "1px 2px");
+		assert_parse!(Inset, "1px 2px 3px 4px");
+	}
+}
