c6d4b39f4b243867bbb77bde95a3b039e7f78006
Keith Cirkel <keithamus@users.noreply.github.com>
2024-12-10 23:19:13 +0000

    (ast) add color_hdr support

--- crates/hdx_ast/src/css/values/color_hdr/impls.rs
+++ crates/hdx_ast/src/css/values/color_hdr/impls.rs
@@ -1,1 +1,20 @@
 pub(crate) use crate::traits::StyleValue;
+pub(crate) use hdx_proc_macro::*;
+
+#[cfg(test)]
+mod tests {
+	use super::super::*;
+	use crate::test_helpers::*;
+
+	#[test]
+	fn size_test() {
+		assert_size!(DynamicRangeLimit, 56);
+	}
+
+	#[test]
+	fn test_writes() {
+		assert_parse!(DynamicRangeLimit, "high");
+		assert_parse!(DynamicRangeLimit, "dynamic-range-limit-mix(high 80%,standard 20%)");
+		assert_parse!(DynamicRangeLimit, "dynamic-range-limit-mix(high 8%,standard 2%)");
+	}
+}
